5854df7c4c79f556fc0e3a8e250bf8ec6809ab84
Royce Li <76494636+rtest42@users.noreply.github.com>
2025-05-16 00:23:43 -0700

    Clean up script.js, use WebStorm IDE

--- script.js
+++ script.js
@@ -1,266 +1,254 @@
 import 'https://tomashubelbauer.github.io/github-pages-local-storage/index.js';
 
-
 const MAX_GUESSES = 5;
 const URL = "https://rtest42.github.io/vta-routle";
-const SHAPES_FILE = "shapes.json";
-const ROUTES_FILE = "routes.json";
-const CENTER_COORDS = [37.3340062,-121.8917829];
+const SHAPES_URL = "shapes.json";
+const ROUTES_URL = "routes.json";
+const CENTER_COORDS = [37.3340062, -121.8917829];
 
 let map;
-let routeLayers = {}; // Store GeoJSON layers for routes
-let routeMap = {}; // Store route names for route numbers
+const routeLayers = {}; // GeoJSON layers for routes
+let routeMap = {}; // Route names for route numbers
 let currentRoute;
 
-// Seed function Mulberry32 PRNG
-function mulberry32(a) {
-    let t = a += 0x6D2B79F5;
+const date = new Date();
+const dateToInt = (date.getFullYear() % 100000) * 10000 + (date.getMonth() + 1) * 100 + date.getDate();
+const checkbox = document.getElementById("hard-mode");
+
+function mulberry32(num) { // Seed function PRNG
+    let t = num + 0x6D2B79F5;
     t = Math.imul(t ^ t >>> 15, t | 1);
     t ^= t + Math.imul(t ^ t >>> 7, t | 61);
     return ((t ^ t >>> 14) >>> 0) / 4294967296;
 }
 
-const date = new Date();
-const dateToInt = (date.getFullYear() % 100000) * 10000 + (date.getMonth() + 1) * 100 + date.getDate(); // Not actually a number that represents today's date but rather a unique number 
-
-function checkFirstVisit(inputDate) {
-    const lastVisit = localStorage.getItem('lastVisit');
-
-    if (inputDate != lastVisit) {
-        // User is visiting for the first time today
-        localStorage.setItem('lastVisit', inputDate);
-
-        // Set game variables
-        localStorage.setItem("guesses", 0);
-        localStorage.setItem("status", "unplay");
-        localStorage.setItem("startTime", performance.now());
-        for (let i = 1; i <= MAX_GUESSES; ++i) {
-            localStorage.setItem(`${i}`, null);
-        }
-    }
-}
-
-// Call the function to check first visit
-checkFirstVisit(dateToInt);
-
-const checkbox = document.getElementById("hard-mode");
-
-// Load routes from shapes.json
-fetch(ROUTES_FILE)
-    .then(response => response.json())
-    .then(data => {
-        routeMap = data;
-        return fetch(SHAPES_FILE);
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Create map
-        map = L.map('map', {
-            center: CENTER_COORDS,
-            zoom: 11,
-            zoomControl: true,
-            dragging: true,
-            scrollWheelZoom: true,
-            doubleClickZoom: true,
-            touchZoom: true
-        });
-
-        // Store the GeoJSON layers for each route
-        for (const shapeId in data) {
-            const coordinates = data[shapeId].map(([lat, lon]) => [lon, lat]);
-
-            const routeGeoJSON = {
-                "type": "FeatureCollection",
-                "features": [{
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "LineString",
-                        "coordinates": coordinates
-                    },
-                    "properties": {
-                        "name": shapeId
-                    }
-                }]
-            };
-
-            // Create the GeoJSON layer for each route
-            routeLayers[shapeId] = L.geoJSON(routeGeoJSON, {
-                style: function () {
-                    return {
-                        color: 'blue',
-                        weight: 3,
-                        opacity: 1
-                    };
-                }
-            });
-        }
-
-        // Select the route of the day
-        currentRoute = getRouteOfTheDay(Object.keys(routeMap));
-
-        // Display the route on the map
-        for (const key of Object.keys(routeLayers)) {
-            if (key.startsWith(`${currentRoute}_`)) {
-                routeLayers[key].addTo(map);
-            }
-        }
-
-        // Create the route selection buttons
-        const buttonContainer = document.getElementById('routeButtons');
-        if (buttonContainer) {
-            for (const [key, value] of Object.entries(routeMap)) {
-                const button = document.createElement('button');
-                button.setAttribute("id", key);
-                if (localStorage.getItem("hard-mode")) {
-                    button.textContent = key;
-                } else {
-                    button.textContent = `${key} ${value.trim().toLowerCase().replace(/\b\w/g, (match) => match.toUpperCase())}`;
-                }
-                button.className = 'route-button';
-                button.onclick = () => checkGuess(key);
-                buttonContainer.appendChild(button);
-            }
-        }
-
-        // Load stored game variables
-        for (let i = 1, n = localStorage.getItem("guesses"); i <= n; ++i) {
-            const square = document.getElementById(`square${i}`);
-            const guessedRoute = localStorage.getItem(`${i}`);
-            if (square && guessedRoute) {
-                square.textContent = guessedRoute;
-                if (guessedRoute == currentRoute) {
-                    square.style.backgroundColor = 'green';
-                } else {
-                    square.style.backgroundColor = 'red';
-                    const button = document.getElementById(guessedRoute);
-                    if (button) button.disabled = true;
-                    for (const key of Object.keys(routeLayers)) {
-                        if (key.startsWith(`${guessedRoute}_`)) {
-                            routeLayers[key].setStyle({ color: 'red' });
-                            routeLayers[key].addTo(map);
-                        }
-                    }
-                }
-            }
-        }
-
-        // Load game states
-        const gameState = localStorage.getItem("status");
-        if (gameState == 'win' || gameState == 'lose') {
-            endGame();
-        }
-        if (gameState != 'unplay') {
-            if (checkbox) checkbox.disabled = true;
-        }
-
-        if (checkbox) checkbox.checked = localStorage.getItem("hard-mode");
-    })
-    .catch(error => console.error("Error loading JSON:", error));
-
-// Function to get a random route id from available routes
 function getRouteOfTheDay(routeIds) {
     const randomIndex = Math.floor(mulberry32(dateToInt) * routeIds.length);
     return routeIds[randomIndex];
 }
 
-// Function to check if the user's guess is correct
-function checkGuess(guessedRoute) {
-    // If the guess is correct, display the route and end the game
-    localStorage.setItem("guesses", parseInt(localStorage.getItem("guesses")) + 1);
-    localStorage.setItem(`${localStorage.getItem("guesses")}`, guessedRoute);
-    const square = document.getElementById(`square${localStorage.getItem("guesses")}`);
-    if (checkbox) checkbox.disabled = true;
-    if (square) {
-        square.textContent = guessedRoute;
-        if (guessedRoute == currentRoute) {
-            square.style.backgroundColor = 'green';
-            localStorage.setItem("endTime", performance.now());
-            localStorage.setItem("status", "win");
-            alert(`Correct! Today's route is ${currentRoute} ${routeMap[currentRoute].trim().toLowerCase().replace(/\b\w/g, (match) => match.toUpperCase())}`);
-            endGame();
-        } else {
-            square.style.backgroundColor = 'red';
-            localStorage.setItem("status", "play");
-            const button = document.getElementById(guessedRoute);
-            if (button) button.disabled = true;
-            for (const key of Object.keys(routeLayers)) {
-                if (key.startsWith(guessedRoute + '_')) {
-                    routeLayers[key].setStyle({ color: 'red' });
-                    routeLayers[key].addTo(map);
-                }
-            }
-            if (localStorage.getItem("guesses") >= MAX_GUESSES) {
-                localStorage.setItem("status", "lose");
-                alert(`You ran out of guesses. Today's route is ${currentRoute} ${routeMap[currentRoute].trim().toLowerCase().replace(/\b\w/g, (match) => match.toUpperCase())}`);
-                endGame();
-            }
+function checkFirstVisitOfTheDay(inputDate) {
+    const lastVisit = localStorage.getItem('lastVisit');
+    if (inputDate !== parseInt(lastVisit || "0")) { // User is visiting for the first time today
+        localStorage.setItem('lastVisit', inputDate.toString());
+        localStorage.setItem("guesses", "0");
+        localStorage.setItem("status", "unplay");
+        localStorage.setItem("startTime", performance.now().toString());
+        for (let i = 1; i <= MAX_GUESSES; ++i) {
+            localStorage.setItem(`${i}`, "");
         }
     }
 }
 
-// Function to end the game
-function endGame() {
-    const buttons = document.querySelectorAll('.button-container .route-button');
-    if (buttons.length) {
-        buttons.forEach(button => {
-            button.disabled = true; // Disable all buttons after game ends
+function formatRouteName(routeName) {
+    return routeName.trim().toLowerCase().replace(/\b\w/g, (match) => match.toUpperCase());
+}
+
+async function initGame() {
+    checkFirstVisitOfTheDay(dateToInt);
+
+    try {
+        const routeRes = await fetch(ROUTES_URL);
+        routeMap = await routeRes.json();
+
+        const shapesRes = await fetch(SHAPES_URL);
+        const shapeData = await shapesRes.json();
+
+        initMap(shapeData);
+        currentRoute = getRouteOfTheDay(Object.keys(routeMap));
+        drawCurrentRoute();
+        setupButtons();
+        loadGameState();
+    } catch (err) {
+        console.error("Error loading JSON:", err);
+    }
+}
+
+function initMap(shapes) {
+    map = L.map('map', {
+        center: CENTER_COORDS,
+        zoom: 11,
+        zoomControl: true,
+        dragging: true,
+        scrollWheelZoom: true,
+        doubleClickZoom: true,
+        touchZoom: true
+    });
+
+    for (const shapeID in shapes) {
+        const coordinates = shapes[shapeID].map(([lat, lon]) => [lon, lat]);
+
+        const geoJSON= {
+            type: "FeatureCollection",
+            features: [{
+                type: "Feature",
+                geometry: {
+                    type: "LineString",
+                    coordinates: coordinates
+                },
+                properties: {
+                    name: shapeID
+                }
+            }]
+        };
+
+        routeLayers[shapeID] = L.geoJSON(geoJSON, {
+            style: { color: 'blue', weight: 3, opacity: 1 }
         });
     }
-    const button = document.getElementById(currentRoute);
-    if (button) {
-        button.style.backgroundColor = 'green';
-        button.style.color = 'white';
+}
+
+function drawCurrentRoute() {
+    for (const key in routeLayers) {
+        if (key.startsWith(`${currentRoute}_`)) {
+            routeLayers[key].addTo(map);
+        }
     }
-    const shareButton = document.getElementById('share-button');
-    if (shareButton) {
-        shareButton.style.display = "block";
+}
+
+function setupButtons() {
+    const container = document.getElementById('routeButtons');
+    if (!container) return;
+
+    for (const [key, value] of Object.entries(routeMap)) {
+        const button = document.createElement('button');
+        button.dataset.routeID = key;
+        button.id = key;
+        button.className = 'route-button';
+        button.onclick = () => checkGuess(key);
+        button.textContent = checkbox?.checked ? key : `${key} ${formatRouteName(value)}`;
+        container.appendChild(button);
     }
-    // Add dark mode tile layer
+}
+
+function loadGameState() {
+    const guesses = parseInt(localStorage.getItem("guesses") || "0")
+    for (let i = 1; i <= guesses; ++i) {
+        const square = document.getElementById(`square${i}`);
+        const guessedRoute = localStorage.getItem(`${i}`);
+        if (square && guessedRoute) {
+            square.textContent = guessedRoute;
+            if (guessedRoute === currentRoute) {
+                square.style.backgroundColor = 'green';
+            } else {
+                square.style.backgroundColor = 'red';
+                const btn = document.getElementById(guessedRoute);
+                if (btn) btn.disabled = true;
+                colorRouteRed(guessedRoute);
+            }
+        }
+    }
+
+    const state = localStorage.getItem("status");
+    if (state === 'win' || state === 'lose') endGame();
+    if (state !== 'unplay' && checkbox) checkbox.disabled = true;
+    if (checkbox) checkbox.checked = localStorage.getItem("hard-mode") === 'true';
+}
+
+function colorRouteRed(routeID) {
+    for (const key in routeLayers) {
+        if (key.startsWith(`${routeID}_`)) {
+            routeLayers[key].setStyle({ color: 'red' });
+            routeLayers[key].addTo(map);
+        }
+    }
+}
+
+function checkGuess(guessedRoute) {
+    const guessNum = parseInt(localStorage.getItem("guesses") || "0") + 1;
+    const displayRoute = `${currentRoute} ${formatRouteName(routeMap[currentRoute])}`;
+    localStorage.setItem("guesses", guessNum.toString());
+    localStorage.setItem(`${guessNum}`, guessedRoute);
+    const square = document.getElementById(`square${guessNum}`);
+    if (checkbox) checkbox.disabled = true;
+    if (!square) return;
+
+    square.textContent = guessedRoute;
+    if (guessedRoute === currentRoute) {
+        square.style.backgroundColor = 'green';
+        localStorage.setItem("endTime", performance.now().toString());
+        localStorage.setItem("status", "win");
+        alert(`Correct! Today's route is ${displayRoute}`);
+        endGame();
+    } else {
+        square.style.backgroundColor = 'red';
+        localStorage.setItem("status", "play");
+        const btn = document.getElementById(guessedRoute);
+        if (btn) btn.disabled = true;
+        colorRouteRed(guessedRoute);
+
+        if (guessNum >= MAX_GUESSES) {
+            localStorage.setItem("status", "lose");
+            alert(`You ran out of guesses. Today's route is ${displayRoute}`);
+            endGame();
+        }
+    }
+}
+
+function endGame() {
+    document.querySelectorAll('.button-container .route-button').forEach(btn => btn.disabled = true);
+
+    const winBtn = document.getElementById(currentRoute);
+    if (winBtn) {
+        winBtn.style.backgroundColor = 'green';
+        winBtn.style.color = 'white';
+    }
+    const shareBtn = document.getElementById('share-button');
+    if (shareBtn) shareBtn.style.display = "block";
+
     L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
         attribution: '&copy; OpenStreetMap contributors, &copy; CartoDB'
     }).addTo(map);
 }
 
 // Function to share results
-function shareResults() {
-    let currentDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-    let hardMode = "";
-    if (localStorage.getItem("hard-mode")) {
-        hardMode = " - Hard Mode";
+async function shareResults() {
+    const currentDate = new Intl.DateTimeFormat(navigator.language).format(new Date());
+    const isHardMode = localStorage.getItem("hard-mode") === 'true';
+    const gameStatus = localStorage.getItem("status");
+    const guessCount = parseInt(localStorage.getItem("guesses") || "0");
+    const hardModeText = isHardMode ? " - Hard Mode" : "";
+    const timeText = getTimeText(gameStatus);
+    const guessDisplay = getGuessDisplay(gameStatus, guessCount);
+
+    try {
+        await navigator.clipboard.writeText(`VTA Historoutle ${currentDate}${hardModeText}${timeText}\n${guessDisplay}\n\n${URL}`);
+        alert("Copied to clipboard!");
+    } catch (err) {
+        alert("Failed to copy text to clipboard!");
+        console.log(err);
     }
-    let time = "";
-    if (localStorage.getItem("status") == 'win') {
-        let elapsedTime = localStorage.getItem("endTime") - localStorage.getItem("startTime");
-        time = ` - ${(elapsedTime / 1000).toFixed(3)}s`;
-    }
-    let displayGuesses = ['⬛ ', '⬛ ', '⬛ ', '⬛ ', '⬛ '];
-    for (let i = 0, n = localStorage.getItem("guesses"); i < n; ++i) {
-        displayGuesses[i] = '🟥 ';
-    }
-    if (localStorage.getItem("status") == "win") {
-        displayGuesses[localStorage.getItem("guesses") - 1] = '🟩 ';
-    }
-    navigator.clipboard.writeText(`VTA Historoutle ${currentDate}${hardMode}${time}\n${displayGuesses.join('')}\n\n${URL}`);
-    alert("Copied to clipboard!");
 }
 
-// Function to toggle hard mode
+function getTimeText(status) {
+    if (status !== 'win') return '';
+    const start = parseFloat(localStorage.getItem("startTime"));
+    const end = parseFloat(localStorage.getItem("endTime"));
+    const elapsedSeconds = ((end - start) / 1000).toFixed(3);
+    return ` - ${elapsedSeconds}s`;
+}
+
+function getGuessDisplay(status, guessCount) {
+    const display = Array(MAX_GUESSES).fill('\u{2B1B} '); // black square
+    for (let i = 0; i < guessCount; ++i) display[i] = '\u{1F7E5} '; // red square
+    if (status === 'win') display[guessCount - 1] = '\u{1F7E9} '; // green square
+    return display.join('');
+}
+
 function toggleHardMode() {
-    localStorage.setItem("hard-mode", checkbox.checked);
-    const buttonContainer = document.getElementById('routeButtons').children;
-    const shapes = Object.keys(routeLayers);
-    if (buttonContainer) {
-        for (let i = 0; i < buttonContainer.length; ++i) {
-            const button = buttonContainer[i];
-            if (checkbox.checked) {
-                button.textContent = button.textContent.slice(0, button.textContent.indexOf(" "));
-            } else {
-                button.textContent = `${button.textContent} ${routeMap[button.textContent].trim().toLowerCase().replace(/\b\w/g, (match) => match.toUpperCase())}`;
-            }
-        }
+    const checked = checkbox.checked;
+    localStorage.setItem("hard-mode", checked.toString());
+    const container = document.getElementById('routeButtons');
+    if (!container) return;
+
+    for (const btn of container.children) {
+        const key = btn.dataset.routeId;
+        btn.textContent = checked ? key : `${key} ${formatRouteName(routeMap[key])}`;
     }
 }
 
-
+// Add event listeners via mouse button for share button and hard mode toggling
 document.getElementById("share-button").addEventListener("click", shareResults);
-checkbox.addEventListener("change", toggleHardMode);
+checkbox?.addEventListener("change", toggleHardMode);
+
+window.onload = async () => { await initGame(); };
